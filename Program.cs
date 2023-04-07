62bc10f02e24f3c9eb1d5293d4a0cb5e00d3a106
MShArAFidDiNM <sharafiddin2007@outlook.com>
2023-04-07 14:55:14 +0500

    task7

--- Program.cs
+++ Program.cs
@@ -10,6 +10,20 @@ namespace Takrorlash
     {
         static void Main(string[] args)
         {
+            Console.Write("Satrni kiriting : ");
+            string str = Console.ReadLine();
+
+            for (int i = 1; i < str.Length; i++)
+            {
+                if (str[i] != ' ')
+                {
+                    Console.WriteLine(str[i]);
+                }
+                else break;
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
