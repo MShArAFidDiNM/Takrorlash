379cfb84eefcbdc58a5b76405b751ecad25b942f
MShArAFidDiNM <sharafiddin2007@outlook.com>
2023-04-07 10:40:32 +0500

    task3

--- Program.cs
+++ Program.cs
@@ -10,6 +10,39 @@ namespace Takrorlash
     {
         static void Main(string[] args)
         {
+            Console.Write("Row : ");
+            string input = Console.ReadLine();
+            int row1;
+            bool check = int.TryParse(input, out row1);
+
+            int row = (row1 / 2)+1;
+            int row2 = (row1 / 2);
+
+            for (int i = 1; i <= row; i++)
+            {
+                for (int j = 1; j <= row - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+            for (int i = row-1; i >= 1; i--)
+            {
+                for (int j = 1; j <= row - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
     }
 }
