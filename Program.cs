f13a88c7c7b903d51d5a9851cce56bf7953f8964
MShArAFidDiNM <sharafiddin2007@outlook.com>
2023-04-07 10:07:14 +0500

    task1

--- Program.cs
+++ Program.cs
@@ -10,6 +10,19 @@ namespace Takrorlash
     {
         static void Main(string[] args)
         {
+            Random rnd = new Random();
+            Console.Write("Enter Row : ");
+            string input = Console.ReadLine(); 
+            int row = int.Parse(input);
+            for (int i = 0; i <= row; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(" * ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
     }
 }
